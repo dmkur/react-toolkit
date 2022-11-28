0883231ad22dcad760699ab4d88c7eab763694f7
dmkur <fgameua@gmail.com>
2022-11-28 20:43:26 +0200

    redux setup

---
+++ src/redux/store.js
@@ -0,0 +1,11 @@
+import {combineReducers, configureStore} from "@reduxjs/toolkit";
+
+const rootReducer = combineReducers({});
+
+const setupStore = () => configureStore({
+    reducer: rootReducer
+});
+
+export {
+    setupStore
+}
